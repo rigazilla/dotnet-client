06434265af9e56222d6d70bc119c7f5ed126362d
Vittorio Rigamonti <rigazilla@gmail.com>
2017-10-03 11:45:41 +0200

    added configuration builder

--- samples/Infinispan.HotRod.SimpleSample/Program.cs
+++ samples/Infinispan.HotRod.SimpleSample/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using Infinispan.HotRod.Config;
 namespace Infinispan.HotRod.SimpleSample
 {
     class Program
     {
+        private static IRemoteCache<String, String> cache;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ConfigurationBuilder conf = new ConfigurationBuilder();
+            conf.AddServer().Host("127.0.0.1").Port(11222);
+            conf.ConnectionTimeout(90000).SocketTimeout(6000);
+            RemoteCacheManager remoteManager = new RemoteCacheManager(conf.Build(), true);
+            cache = remoteManager.GetCache<String, String>();
         }
     }
 }
