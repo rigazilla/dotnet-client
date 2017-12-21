22e501b0732258cb814afbd7478b221e46fb99a2
Vittorio <rigazilla@gmail.com>
2017-12-22 00:00:45 +0100

    HRCPP-440 added new files for .NET Core build

---
+++ samples/Infinispan.HotRod.SimpleSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Infinispan.HotRod.Config;
+
+namespace Infinispan.HotRod.SimpleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (var key, var value) = ("hello", "world!");
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddServer().Host("127.0.0.1").Port(11222);
+            configurationBuilder.ConnectionTimeout(90000).SocketTimeout(6000);
+            var remoteManager = new RemoteCacheManager(configurationBuilder.Build());
+            var cache = remoteManager.GetCache<string, string>();
+            cache.PutIfAbsent(key, value);
+            var outOfCache = cache.Get(key);
+            Console.WriteLine($"{key}: {outOfCache}");
+            Console.ReadKey();
+        }
+    }
+}
